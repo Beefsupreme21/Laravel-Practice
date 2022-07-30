756e56d5b77b012baad9eb53007e138af77a4605
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-07-30 16:26:53 -0600

    Add files via upload

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -10,12 +10,16 @@
     @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/posts/{{ $post->id }}">
+                <a href="/posts/{{ $post->slug }}">
                 <?= $post->title; ?>
 
                 </a>
                 {{ $post->title }}
             </h1>
+
+            <p>
+                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+            </p>
 
             <div>
                 <?= $post->excerpt; ?>
