8629d5720b2f3e9ee8a06b5ee0424e421447633d
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-07-30 16:27:21 -0600

    Add files via upload

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,48 +1,17 @@
-<?php 
+<?php
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public $title; 
-    public $excerpt; 
-    public $date; 
-    public $body;
-    public $slug;
+    use HasFactory;
+    protected $fillable = ['title', 'excerpt', 'body', 'slug', 'category_id'];
 
-    public function __construct($title, $excerpt, $date, $body, $slug)
+    public function category() 
     {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
-
-    public static function all()
-    {   
-        return collect(File::files(resource_path("posts")))
-        ->map(fn($file) => YamlFrontMatter::parseFile($file))
-        ->map(fn($document) => new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        ));
-
-    }
-
-    public static function find($slug)
-    {
-
-        $posts = static::all();
-
-        return $posts->firstWhere('slug', $slug);
-
+        return $this->belongsTo(Category::class);
     }
 }
