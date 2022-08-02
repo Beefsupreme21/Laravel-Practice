6f43cec3b23a43a65026614abf4ed16bae66fe91
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-08-02 16:16:46 -0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\Translation\Dumper\YamlFileDumper;
@@ -18,13 +19,10 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $posts = Post::all();
-
     return view('posts', [
-        'posts' => $posts
+        'posts' => Post::latest()->with('category', 'author')->get()
     ]);
 });
-
 
 
 Route::get('posts/{post:slug}', function (Post $post) {
@@ -33,8 +31,17 @@ Route::get('posts/{post:slug}', function (Post $post) {
     ]);
 });
 
+
+
+
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts->load(['category', 'author'])
+    ]);
+});
+
+Route::get('authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'posts' => $author->posts->load(['category', 'author'])
     ]);
 });
