ccf933680ff0c8d6de0c0317634230ff5274ff61
Cory S <101227906+Beefsupreme21@users.noreply.github.com>
2022-08-04 22:35:38 -0600

    Add files via upload

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,10 +1,9 @@
 <x-layout>
-    
     <x-post-header :categories=$categories/>
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())     
-            {{-- <x-posts-grid :posts="$posts"/> --}}
+            <x-posts-grid :posts="$posts"/>
         @else
             <p class="text-center">No posts yet.</p>
         @endif
